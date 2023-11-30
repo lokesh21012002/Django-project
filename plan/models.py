825c03f3ffd92c7f4d171fa3ed3907af459a9695
Lokesh Gawande <lokesh@Lokeshs-MacBook-Pro.local>
2023-12-01 02:33:58 +0530

    destination field i  plan model

--- plan/models.py
+++ plan/models.py
@@ -7,6 +7,7 @@ class Plan(models.Model):
     id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=20)
     description = models.TextField()
+    destination = models.CharField(max_length=20, default='destination')
 
     def __str__(self):
         return self.name
