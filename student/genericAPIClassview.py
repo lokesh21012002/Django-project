0b2af9c3c1eee707be288c48b5bd0dc78f04f53d
Lokesh Gawande <lokesh@Lokeshs-MacBook-Pro.local>
2023-12-13 00:02:46 +0530

    Concrete view class

--- student/genericAPIClassview.py
+++ student/genericAPIClassview.py
@@ -1,23 +1,24 @@
-from rest_framework.generics import GenericAPIView
+from rest_framework.generics import GenericAPIView, CreateAPIView, UpdateAPIView, DestroyAPIView, ListAPIView
 from rest_framework.mixins import ListModelMixin, CreateModelMixin, UpdateModelMixin, DestroyModelMixin
 
 from .models import Student
 from .serializers import Studentserializers
 
 
-class CommonView(GenericAPIView, ListModelMixin, CreateModelMixin, UpdateModelMixin, DestroyModelMixin):
+# class CommonView(GenericAPIView, ListModelMixin, CreateModelMixin, UpdateModelMixin, DestroyModelMixin):
+class CommonView(CreateAPIView, UpdateAPIView, DestroyAPIView, ListAPIView):
     queryset = Student.objects.all()
     serializer_class = Studentserializers
 
-    def get(self, request, *args, **kargs):
-        return self.list(request, *args, **kargs)
+    # def get(self, request, *args, **kargs):
+    #     return self.list(request, *args, **kargs)
 
-    def post(self, request, *args, **kargs):
-        return self.create(request, *args, **kargs)
+    # def post(self, request, *args, **kargs):
+    #     return self.create(request, *args, **kargs)
 
-    def put(self, request, *args, **kargs):
-        return self.update(request, *args, **kargs)
+    # def put(self, request, *args, **kargs):
+    #     return self.update(request, *args, **kargs)
 
-    def delete(self, request, *args, **kargs):
+    # def delete(self, request, *args, **kargs):
 
-        return self.destroy(request, *args, **kargs)
+    #     return self.destroy(request, *args, **kargs)
--- student/urls.py
+++ student/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path, include
 from .views import *
 from .apiview import CRUD_API
+from .genericAPIClassview import CommonView
 
 urlpatterns = [
     path('student/all/', getAllStudents),
@@ -8,5 +9,7 @@ urlpatterns = [
     path('student/add/', addStudent),
     path('student/delete/<int:id>/', deleteStudent),
     path('student/update/<int:id>/', updateStudent),
-    path('drf/<int:stu_id>/', CRUD_API)
+    path('drf/<int:stu_id>/', CRUD_API),
+    path("generic/", CommonView.as_view())
+
 ]
