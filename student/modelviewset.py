cfe914c07de0056824416648ab206255fc15d3ec
Lokesh Gawande <lokesh@Lokeshs-MacBook-Pro.local>
2023-12-13 21:35:30 +0530

    Basic Auth

--- student/modelviewset.py
+++ student/modelviewset.py
@@ -1,11 +1,23 @@
 from rest_framework import viewsets
 from .models import Student
 from .serializers import Studentserializers
+from rest_framework import authentication, authtoken
+from rest_framework.permissions import IsAuthenticated, AllowAny
+from rest_framework.authentication import BaseAuthentication
 
 
 class viewSetStudent(viewsets.
                      #  ModelViewSet
                      ReadOnlyModelViewSet
                      ):
+    authentication_classes = [BaseAuthentication]
+    permission_classes = [IsAuthenticated]
     queryset = Student.objects.all()
     serializer_class = Studentserializers
+
+
+class AuthViewSet(viewsets.ModelViewSet):
+    authentication_classes = [BaseAuthentication]
+    permission_classes = [AllowAny]  # override from global settings
+    serializer_class = Studentserializers
+    queryset = Student.objects.all()
--- travel_plan/settings.py
+++ travel_plan/settings.py
@@ -39,6 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
 
+
+
     'rest_framework',
     'student'
 
@@ -148,3 +150,12 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': ['rest_framework.authentication.BasicAuthencation'],
+    'DEFAULT_PERMISSION_CLASSES': ['rest_framework.permissions.IsAuthenticated']
+
+
+
+
+}
