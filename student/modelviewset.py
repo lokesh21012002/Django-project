7af6dd3f442353144cfb4a46cd8bf286af6691d9
Lokesh Gawande <lokesh@Lokeshs-MacBook-Pro.local>
2023-12-13 22:55:11 +0530

    Custom auth with token

--- student/modelviewset.py
+++ student/modelviewset.py
@@ -3,21 +3,23 @@ from .models import Student
 from .serializers import Studentserializers
 from rest_framework import authentication, authtoken
 from rest_framework.permissions import IsAuthenticated, AllowAny
-from rest_framework.authentication import BaseAuthentication
+from rest_framework.authentication import BaseAuthentication, TokenAuthentication
+from .customauth import JWTAuthentication
 
 
 class viewSetStudent(viewsets.
                      #  ModelViewSet
                      ReadOnlyModelViewSet
                      ):
-    authentication_classes = [BaseAuthentication]
+    # authentication_classes = [TokenAuthentication]
+    authentication_classes = [JWTAuthentication]
     permission_classes = [IsAuthenticated]
     queryset = Student.objects.all()
     serializer_class = Studentserializers
 
 
 class AuthViewSet(viewsets.ModelViewSet):
-    authentication_classes = [BaseAuthentication]
+    authentication_classes = [TokenAuthentication]
     permission_classes = [AllowAny]  # override from global settings
     serializer_class = Studentserializers
     queryset = Student.objects.all()
